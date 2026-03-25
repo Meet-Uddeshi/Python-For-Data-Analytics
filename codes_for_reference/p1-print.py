5a1c54261edf26e097df40d74792d4cb9b07555b
Navya Admin <hardik_shah71290@yahoo.com>
2026-03-25 10:37:14 +0530

    print and var_type basics

---
+++ codes_for_reference/p1-print.py
@@ -0,0 +1,12 @@
+# Program 1 - Print code
+"""This is first lecture
+lecture is Print statement
+"""
+
+print('''Hello World
+This is first lecture
+it was about Python basics''')
+
+print(""" Cuneiform is one of the oldest surviving forms of writing. It was used in Mesopotamia to compose a tremendous number of texts on clay tablets, thousands of which survive to the present day. 
+Cuneiform had no paragraphs or even spacing between words with the symbols all written directly adjacent to one another.[1] Egyptian hieroglyphics were also unspaced,[2] but as they wrote on papyrus with ink, Egyptians scribes introduced the practice of rubrication: the scribes used black ink for the main body text and contrasting red ink for headings""")
+
---
+++ codes_for_reference/p2-variable.py
@@ -0,0 +1,8 @@
+# can start with special characters, no numeric start, no special char, no space - for variable
+
+var_1="Hardik-Shah"
+var_2=35
+var_3=999999
+var_4=True
+var_5=35.6
+var_6='@'

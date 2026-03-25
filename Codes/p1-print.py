1d82e0b6efbd255a0c45bdf6359b9acd6479f035
aditi m <madariaditi75@gmail.com>
2026-03-25 10:36:32 +0530

    print and varibale basics

---
+++ Codes/p1-print.py
@@ -0,0 +1,10 @@
+# print syntax l1
+print('''Whispers of the Wind
+The sunlight dances on the stream,
+A golden, soft, and gentle dream.
+The trees, they whisper to the breeze,
+And put the anxious mind at ease.
+In every leaf and stone I see,
+A quiet, perfect symphony. 
+''')
+
---
+++ Codes/p2-variables.py
@@ -0,0 +1,7 @@
+# no special character, numerics, no space - variables name
+v1 = 1
+v2 = "aditi"
+v3 = 21.4
+v4 = False #True
+v5 = 'A'
+v6 = 9173359036

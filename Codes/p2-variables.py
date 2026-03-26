d7d50361119e4a54ab7526853815290d6c6c208c
aditi m <madariaditi75@gmail.com>
2026-03-26 10:47:50 +0530

    -print and varibale basics -escape character, input function, r-string, s-string

--- Codes/p2-variables.py
+++ Codes/p2-variables.py
@@ -1,7 +1,28 @@
 # no special character, numerics, no space - variables name
-v1 = 1
-v2 = "aditi"
-v3 = 21.4
-v4 = False #True
-v5 = 'A'
-v6 = 9173359036
+
+#escape character,
+print("sunday\nmonday\ntuesday\nwednesday\nthursday\nfriday\nsaturday")
+print("\taditi is cool")
+
+# input function
+print("what is your name")
+v7 = 741
+# list of function- int, float, long, char
+
+
+# name= input("enter your name: ")
+# surname= input("enter your surname:")
+# p_num= int(input("enter your phone number:"))
+# age= int(input("enter your age:"))
+# marks= float(input("enter your marks:"))
+#
+# print (f"here is your details- {name} {surname} {p_num} {age} {marks}")
+#
+#
+# # r-string & s-string
+# print(f"{v1}\n{v2}\n{v3}\n{v4}\n{v5}\n{v6}")  #to print variable's value
+# print (r"\n")  # to ignore escape character
+#  #print (r"The ampersand sign (&) is a special character representing the word "" originating as a ligature of the Latin )
+
+print ("value of variable 7", v7)
+#if using f- string then use {variable} and if not then use ,

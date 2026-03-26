c127ff4986b7a236dc47a57ba21769e80d323be0
Navya Admin <hardik_shah71290@yahoo.com>
2026-03-26 10:46:39 +0530

    f strings and escape char and input

--- codes_for_reference/p2-variable.py
+++ codes_for_reference/p2-variable.py
@@ -1,8 +1,47 @@
 # can start with special characters, no numeric start, no special char, no space - for variable
+# escape character: (\n) new line,paragraph(\t),r string, f string, input function ctrl+/ - for all selected comment
+# if datatype is string then too it will take number or integers as strings.
+# if f string is defined then we have to put in {}
 
-var_1="Hardik-Shah"
-var_2=35
-var_3=999999
-var_4=True
-var_5=35.6
+
+# var_1="Hardik-Shah"
+# var_2=35
+# var_3=999999
+# var_4=True
+# var_5=35.6
 var_6='@'
+
+print("Value of variable 6 is ",var_6 ,"Hardik")
+
+# (\n) new line,paragraph(\t),r string, f string, input function:
+# print("Hello\nHie")
+#
+#
+# print("\tGood Morning.We have a sunny day today") #new paragraph
+#
+# print(f"{var_1}\n{var_2}\n{var_3}\n{var_4}\n{var_5}\n{var_6}") #f string and new line
+#
+# print(r"\n") #r string used to ignore the f string
+#
+# print(r"Use respectful language, generally preferring person-first language (person  a disability") #r string for ignoring escape char
+
+# print("What is your name?")
+# var7=input()
+# print(f"{var7}") # variable can store the value n times and it will get overlap
+
+#int,float,long,char functions
+
+# f_name = input("Enter your first name: ")
+# phone_no = int(input("Enter your phone number: "))
+# price = float(input("Enter your price: "))
+# blood_group = input("Enter your blood group: ")
+
+
+# f_name = input("Enter Your first name: ")
+# l_name = input("Enter Your last name: ")
+# age = int(input("Enter your age: "))
+# phone_number = int(input("Enter your phone number: "))
+# marks = float(input("Enter your marks: "))
+
+# print(f"here are your details: {f_name} {l_name} {age} {phone_number} {marks}")
+

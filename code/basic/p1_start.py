330f1f3f0054fd911162a29f3f9a47fb9752c940
Meet Uddeshi <uddeshimeet@gmail.com>
2026-03-30 23:13:21 +0530

    Python bsics and getting started.

---
+++ code/basic/p1_start.py
@@ -0,0 +1,9 @@
+# Topic: Print function is used for getting output which prints in terminal.
+print("Hello World")
+
+# Topic: Comments. Comments must be ignored in codebase. it is use for code documentation.
+# One line comment = #
+# Multiline comment = '''...''' or """..."""
+# This is one line comment
+'''This is multiline comment
+This is multiline comment'''

b1b480b97c98e5d6ae79cf2cea80b2018ad80110
Meet Uddeshi <uddeshimeet@gmail.com>
2026-04-02 22:12:51 +0530

    While loop (entry control loop) with multiple examples.

--- code/basic/p5_loop.py
+++ code/basic/p5_loop.py
@@ -5,7 +5,36 @@
 # 3) Increment or decrement operators
 
 # Entry control loop: There are two types of entry control loops for adn while loops. In this type of loop instantaneous variable checks condition and then execute the loop if condition is true. It execute minimum zero time.
-# ------- Code for entry control loop ---------
+
+# 1) while loop:
+i = 0
+while i <= 10:
+    print(i)
+    i = i + 1 # (i+= 1) or (i = i+1)
+
+# Example: Factorial
+i = 0
+n = int(input("Enter a number: "))
+fact = 1
+while i <= n:
+    if n==0 or n==1:
+        fact = 1
+    else:
+        fact = fact * i
+    i = i + 1
+
+# Example: Print only factors of 5 till 50.
+i = 1
+while i<=50:
+    if i % 5 == 0:
+        print(i)
+    i = i + 1
+
+# Example: Print by decrement operators
+i = 100
+while i>=0:
+    print(i)
+    i = i - 1
 
 # Exit control loop: Do..while loop is included in this type of loop. Exit control loop must execute st least one time because it execute first and then check condition for instantaneous variable.
 # ---------- Code for exit control loop ----------

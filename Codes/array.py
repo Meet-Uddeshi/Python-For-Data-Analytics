d3e68d4200d903069e34932fe8fcfc8d57075305
aditi m <madariaditi75@gmail.com>
2026-04-09 10:43:07 +0530

    array operations

--- Codes/array.py
+++ Codes/array.py
@@ -4,15 +4,16 @@
 #length always = number of elements
 #length = index+1
 #array allows duplication- length, position, index is countable
+#length- numnber of elements
 
-a= ["xyz", "aditi", 1, "madari", 32.68, 1]
-print(a)
-i = 0
-while i<len(a):
-    if a[i] == "aditi":
-        a[i]= "meet"
-    i+=1
-print(a)
+# a= ["xyz", "aditi", 1, "madari", 32.68, 1]
+# print(a)
+# i = 0
+# while i<len(a):
+#     if a[i] == "aditi":
+#         a[i]= "meet"
+#     i+=1
+# print(a)
 #mutable- if value is known then use if
 # Or if value is not known then use indexing - i== 1
 
@@ -29,5 +30,40 @@ print(a)
 #         print("Position is ", i+1)
 #         print("Element is ",a[i])
 #         break
-#     i=i+1
+#     i=i+1ti
+#
+# a=["madari", "aditi", "literature", 12345, 56.90, "mandvi"]
+## a.insert (5,"kutch")
+# # print(a)
+# print(a)
+# while True:
+#     k = input("do you want to add another value?: yes/no ")
+#     if k=="no":
+#         print("thank you!")
+#         print(a)
+#         break
+#     else:
+#         i = input("what value you want to add?: ")
+#         j = int(input("where you want to add?: "))
+#         a.insert(j, i)
+# a=[]
+# m=[]
+# h=[]
+# while True:
+#    ques=input("in which part you want to add value? a/m/h: ")
+#    i = input("what value you want to add?: ")
+#    if ques == "a":
+#      print(a.append(i))
+#      print(a)
+#
+#    elif ques == "m":
+#        print(m.append(i))
+#        print(m)
+#
+#    elif ques == "h":
+#        print(h.append(i))
+#        print(h)
+#    else:
+#        print("thanks!")
+#        break
 

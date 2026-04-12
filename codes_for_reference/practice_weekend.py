7b7e9d6973008563f07afcd577bdfa65d3917b90
Hardik <hardik_shah71290@yahoo.com>
2026-04-12 20:06:48 +0530

    weekend practice

---
+++ codes_for_reference/practice_weekend.py
@@ -0,0 +1,97 @@
+# # Q1: Check if a number is positive, negative, or zero
+#
+# number = int(input("Enter the number: "))
+# if number < 0:
+#     print("Entered number is negative")
+# elif number > 0:
+#     print("Entered number is positive")
+# else:
+#     print("Number is zero")
+#
+# #Q2: Check if a number is even or odd
+# number = int(input("Enter the number: "))
+# if number % 2:
+#     print("Entered number is even")
+# else:
+#     print("Entered number is odd")
+
+#Q3: Find the largest of two numbers
+# a = int(input("Enter the first number: "))
+# b = int(input("Enter the second number: "))
+# if a > b:
+#     print("Entered number is larger than second number")
+# else:
+#     print("Entered number is smaller than second number")
+
+#Check if a person is eligible to vote AND has ID
+
+# age = int(input("Enter your age: "))
+# id_holder= input("Do you hold id(Y/N)?:")
+# if age >=18:
+#     if id_holder == "Y":
+#         print("you are eligible for voting")
+#     else:
+#         print("Get id first")
+# else:
+#     print("You are not eligible for voting")
+
+# Q5: Loan eligibility check
+#
+# salary = int(input("Enter your salary per annum: "))
+# credit_score = int(input("Enter your credit score: "))
+# if salary > 200000:
+#     if credit_score > 600:
+#         print("You are eligible for the loan")
+#     else:
+#         print("Your credit score is too low")
+# else:
+#     print("Your salary is low for loan application")
+#
+
+# Print numbers from 1 to 10 using while loop
+#
+# i=1
+# while i<=10:
+#     print (i)
+#     # i=i+1
+
+# Print even numbers from 1 to 20
+#
+# i=0
+# while i<=20:
+#     print (i)
+#     i=i+2
+
+# # Number between 10 and 50
+# num = int(input("Enter number: "))
+#
+# if num > 10 and num < 50:
+#     print("in range (10 - 50)")
+# else:
+#     print("out of range")
+
+# Number divisble by 3 and 5
+# num = int(input("Enter number: "))
+#
+# if num %3==0 and num %5==0:
+#     print("divisible by 3 & 5")
+# else:
+#     print("not divisible by 3 & 5")
+#
+# num = int(input("Enter number: "))
+
+# Number divisble by 2 or 5
+#
+# if num %2==0 or num %5==0:
+#     print("divisible by 2 or 5")
+# else:
+#     print("not divisible by 2 or 5")
+
+# # Voting eligiblity
+# age = int(input("Enter age: "))
+# citizen = input("Are you a citizen (Yes/No): ")
+#
+# if age >= 18 and citizen == "Yes":
+#     print("Eligible to vote")
+# else:
+#     print("Not eligible")

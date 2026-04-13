92815c861a4b67f2d6716dda0a48336997de2643
aditi m <madariaditi75@gmail.com>
2026-04-13 10:54:35 +0530

    array learnings

--- Codes/array.py
+++ Codes/array.py
@@ -6,6 +6,26 @@
 #array allows duplication- length, position, index is countable
 #length- numnber of elements
 
+# Array iterations. For this process must need to use loops. Any operations in array must be applied on array index.
+i = 0
+a = [1,2,3,4,5,7,6,"meet"]
+while i<len(a):
+    print(a[i])
+    i += 1
+
+j = 0
+while j<len(a):
+    if a[j] == "meet":
+        print(j)
+    j = j + 1
+
+# Array element insertion. Array functions append and insert used.
+m = [2,3,4,5,6,7,8,9]
+print(m)
+m.append(10)    # Append function/method is used to add new element in array runtime at last index.
+print(m)
+m.insert(3,"aditi")   # Insert function/method is used to add new element in array at specific index and if index is not define then this function takes last index by default to add new element in array.
+
 # a= ["xyz", "aditi", 1, "madari", 32.68, 1]
 # print(a)
 # i = 0

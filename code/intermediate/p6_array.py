ecc217a2a54e9bd45f696c0d4287498307801242
Hardik <hardik_shah71290@yahoo.com>
2026-04-22 10:30:58 +0530

    minor change for matrix multiplication

--- code/intermediate/p6_array.py
+++ code/intermediate/p6_array.py
@@ -66,7 +66,7 @@ c = [[0, 0],
 for i in range(len(a)):
     for j in range(len(b[0])):
         for k in range(len(b)):
-            c[i][j] += a[i][k] * a[k][j]
+            c[i][j] += a[i][k] * b[k][j]
 
 for row in c:
     print(row)

300c8c070d6f083599f850fac0b89fdd7ece27f5
Hardik <hardik_shah71290@yahoo.com>
2026-05-04 10:31:27 +0530

    OOP classes and object creation

---
+++ codes_for_reference/p10-OOP.py
@@ -0,0 +1,38 @@
+# OOP stands for Object-Oriented Programming.
+# Python is an object-oriented language, allowing you to structure your code using classes and objects for better organization and reusability.
+# blue print of all functions.
+# Advantages of OOP
+# Provides a clear structure to programs
+# Makes code easier to maintain, reuse, and debug
+# Helps keep your code DRY (Don't Repeat Yourself)
+# Allows you to build reusable applications with less code
+# 4 pillars of OOP:
+#
+# Python Inheritance
+# Polymorphism
+# Encapsulation
+# Abstrction
+
+
+
+# Classes and objects are the two core concepts in object-oriented programming.
+#
+# A class defines what an object should look like, and an object is created based on that class. For example:
+#
+# Class - Objects
+# Fruit - Apple, Banana, Mango
+# Car - Volvo, Audi, Toyota
+#
+# in the above fruit and car is class defined and Apple, banana, AUdi etc is objects and Shimla apple, kashmiri apple
+# are the properties
+# Objects and properties can be changed but class cannot be changed.
+
+# create class:
+
+# class cars:
+#     x = ("Volvo", "Audi", "Toyota")
+#
+# y =cars()
+# print(y.x)
+
+

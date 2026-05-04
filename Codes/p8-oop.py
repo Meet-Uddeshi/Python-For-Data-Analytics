f5279466c023128fcf870a1a67868bcc44d3c4d1
aditi m <madariaditi75@gmail.com>
2026-05-04 10:46:39 +0530

    opp- class, object and its properties with example

---
+++ Codes/p8-oop.py
@@ -0,0 +1,17 @@
+#oop- object oriented programme
+# Provides a clear structure to programs
+# Makes code easier to maintain, reuse, and debug
+# Helps keep your code DRY (Don't Repeat Yourself)
+# Allows you to build reusable applications with less code
+#Classes - it is a group of variables, methods, functions and so on.... ex- fruits
+#object- it is instance of class    ex-Apple, Banana, Mango
+#Almost everything in Python is an object, with its properties and methods.
+#A Class is like an object constructor, or a "blueprint" for creating objects.
+
+#objects can be changed based on its properties but class can not be changed.
+class aditi:
+    fruits= ("apple", "banana", "orange")
+
+a1=aditi()
+print(a1.fruits)
+
